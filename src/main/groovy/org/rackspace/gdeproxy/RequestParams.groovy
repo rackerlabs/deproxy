cea3285c2a6e2b9ba951d5dbf58458fd5507dcb0
Richard Sartor <richard.sartor@rackspace.com>
2013-07-30 12:49:01 -0500

    Added the RequestParams class, a client-side corollary to HandlerContext for passing additional optional info to the client connector.

---
+++ src/main/groovy/org/rackspace/gdeproxy/RequestParams.groovy
@@ -0,0 +1,11 @@
+package org.rackspace.gdeproxy
+/**
+ * Created with IntelliJ IDEA.
+ * User: izrik
+ *
+ */
+
+class RequestParams {
+    boolean usedChunkedTransferEncoding = false;
+    boolean sendDefaultRequestHeaders = true;
+}

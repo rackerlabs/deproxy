9f7bae89a4c28d9be255b21622cf971e46a04b78
Richard Sartor <richard.sartor@rackspace.com>
2013-07-31 16:44:50 -0500

    Made the body field a def instead of String. Cleaned up default value calculation.

--- src/main/groovy/org/rackspace/gdeproxy/Response.groovy
+++ src/main/groovy/org/rackspace/gdeproxy/Response.groovy
@@ -1,75 +1,71 @@
 package org.rackspace.gdeproxy
-//class Response:
-//    """A simple HTTP Response, with status code, status message, headers, and
-//body."""
 
 class Response {
+
+    /*
+     * A simple HTTP Response, with status code, status message, headers, and
+     * body.
+     *
+     */
 
     String code
     String message
     HeaderCollection headers
-    String body
+    def body
 
-    //    def __init__(self, code, message=None, headers=None, body=None):
-  //        """
-  //Parameters:
-  //
-  //code - A numerical status code. This doesn't have to be a valid HTTP
-  //status code; 600+ values are acceptable also.
-  //message - An optional message to go along with the status code. If
-  //None, a suitable default will be provided based on the given status
-  //.code If ``code`` is not a valid HTTP status code, then the default
-  //is the empty string.
-  //headers - An optional collection of name/value pairs, either a mapping
-  //object like ``dict``, or a HeaderCollection. Defaults to an empty
-  //collection.
-  //body - An optional response body. Defaults to the empty string.
-  //"""
-  public Response(code, message=null, headers=null, body=null){
+    public Response(code, message=null, headers=null, body=null) {
 
-    code = code.toString()
-    //
-    //        if message is None:
-    //            if code in message_by_response_code:
-    //                message = message_by_response_code[code]
-    //            elif int(code) in message_by_response_code:
-    //                message = message_by_response_code[int(code)]
-    //            else:
-    //                message = ''
-    if (message == null) {
-      if (HttpResponseMessage.messagesByResponseCode.containsKey(code)){
-        message = HttpResponseMessage.messagesByResponseCode[code]
-      } else {
-        message = ""
-      }
-    }
-    //
-    //        if headers is None:
-    //            headers = {}
-    if (headers == null) {
-      headers = [:]
-    }
-    //
-    //        if body is None:
-    //            body = ''
-    if (body == null) {
-      body = ""
-    }
-    //
-    //        self.code = str(code)
-    //        self.message = str(message)
-    //        self.headers = HeaderCollection(headers)
-    //        self.body = str(body)
-    this.code = code
-    this.message = message.toString()
-    this.headers = new HeaderCollection(headers)
-    this.body = ( body ? body.toString() : "" )
-  }
+        """Parameters:
 
-  @Override
-  String toString() {
-    sprintf('Response(code=%s, message=%s, headers=%s, body=%s)', code, message, headers, body)
-  }
+        code - A numerical status code. This doesn't have to be a valid HTTP
+            status code; for example, values >= 600 are acceptable also, as
+            are non-numbers.
+
+        message - An optional message to go along with the status code. If
+            null, a suitable default will be provided based on the given
+            status code. If ``code`` is not a valid HTTP status code, then
+            the default is the empty string.
+
+        headers - An optional collection of name/value pairs, either a mapping
+            object like ``['name': 'value']``, or a HeaderCollection. Defaults
+            to an empty map.
+
+        body - An optional request body. Defaults to the empty string. Both
+            strings and byte arrays are acceptable. All other types are
+            toString'd.
+        """
+
+        code = code.toString()
+
+        if (message == null) {
+            if (HttpResponseMessage.messagesByResponseCode.containsKey(code)) {
+                message = HttpResponseMessage.messagesByResponseCode[code]
+            } else {
+                message = ""
+            }
+        }
+
+        if (headers == null) {
+            headers = [:]
+        }
+
+        if (body == null) {
+            body = ""
+        } else if (!(body instanceof byte[]) &&
+                   !(body instanceof String)) {
+            body = body.toString()
+        }
+
+        this.code = code
+        this.message = message.toString()
+        this.headers = new HeaderCollection(headers)
+        this.body = (body ? body.toString() : "")
+    }
+
+    @Override
+    String toString() {
+        sprintf('Response(code=%s, message=%s, headers=%s, body=%s)', code, message, headers, body)
+    }
 }
 
 

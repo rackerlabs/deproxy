2fff57ed94e8fbcbee87102360e5ae558f86a493
Richard Sartor <richard.sartor@rackspace.com>
2013-08-16 10:00:02 -0500

    Add the writeBodyChunked method.

--- src/main/groovy/org/rackspace/gdeproxy/BodyWriter.groovy
+++ src/main/groovy/org/rackspace/gdeproxy/BodyWriter.groovy
@@ -2,6 +2,12 @@ package org.rackspace.gdeproxy
 
 import groovy.util.logging.Log4j
 
+import java.nio.ByteBuffer
+import java.nio.CharBuffer
+import java.nio.charset.Charset
+import java.nio.charset.CharsetEncoder
+import java.nio.charset.CodingErrorAction
+
 /**
  * Created with IntelliJ IDEA.
  * User: izrik
@@ -22,20 +28,89 @@ class BodyWriter {
                     writer.write(body)
                     writer.flush()
                 }
+
             } else if (body instanceof byte[]) {
+
                 log.debug("sending binary body, length ${body.length}")
                 log.debug(body.toString())
                 if (body.length > 0) {
                     outStream.write(body)
                     outStream.flush()
                 }
+
             } else {
-                throw new UnsupportedOperationException("Unknown data type in request body")
+
+                throw new UnsupportedOperationException("Unknown data type in message body")
             }
+
         } else {
             log.debug("No body to send");
         }
 
         outStream.flush();
+    }
+
+    def static void writeBodyChunked(body, OutputStream outStream) {
+
+        // see rfc 2616, section 3.6.1
+
+        Charset asciiCharset = Charset.forName("US-ASCII")
+        CharsetEncoder asciiEncoder = asciiCharset.newEncoder()
+        asciiEncoder.onMalformedInput(CodingErrorAction.REPORT)
+        asciiEncoder.onUnmappableCharacter(CodingErrorAction.REPORT)
+
+        ByteBuffer buffer
+        if (body instanceof String) {
+
+            buffer = asciiEncoder.encode(CharBuffer.wrap(body))
+
+        } else if (body instanceof byte[]) {
+
+            buffer = ByteBuffer.wrap(body)
+
+        } else {
+
+            throw new UnsupportedOperationException("Unknown data type in message body")
+        }
+
+        PrintWriter writer = new PrintWriter(outStream);
+        int maxChunkDataSize = 4096; // in octets
+        byte[] bytes = new byte[maxChunkDataSize]
+        int i = 0;
+
+        // *chunk
+        while (i < buffer.limit()) {
+            int nbytes = Math.min(buffer.remaining(), maxChunkDataSize);
+            // chunk-size
+            writer.write(Integer.toHexString(nbytes));
+            // [ chunk-extension ] will go here in the future
+            // CRLF
+            writer.write("\r\n");
+            writer.flush();
+
+            // chunk-data
+            buffer.get(bytes, 0, nbytes)
+            outStream.write(bytes, 0, nbytes);
+            outStream.flush();
+
+            // CRLF
+            writer.write("\r\n");
+            writer.flush();
+            i += nbytes;
+        }
+
+        // last-chunk
+        // 1*("0")
+        writer.write("0");
+        // [ chunk-extension ] will go here in the future
+        // CRLF
+        writer.write("\r\n");
+        writer.flush();
+
+        // trailer will go here in the future
+
+        // CRLF
+        writer.write("\r\n");
+        writer.flush();
     }
 }

a1b39332be09001b7de9f1bfa8441a6d632de569
Richard Sartor <richard.sartor@rackspace.com>
2013-08-16 11:14:00 -0500

    Cruft removal.

--- src/main/groovy/org/rackspace/gdeproxy/HandlerContext.groovy
+++ src/main/groovy/org/rackspace/gdeproxy/HandlerContext.groovy
@@ -1,12 +1,5 @@
 package org.rackspace.gdeproxy
 
-/**
- * Created with IntelliJ IDEA.
- * User: rich4632
- * Date: 7/24/13
- * Time: 12:32 PM
- * To change this template use File | Settings | File Templates.
- */
 class HandlerContext {
 
 //    boolean requestWasChunked;

909abfc6d0e88c3d23b8f682bb9b6993c87fd493
Richard Sartor <richard.sartor@rackspace.com>
2013-10-18 17:06:46 -0500

    Add connector example.

---
+++ src/examples/DisconnectConnectorTest.groovy
@@ -0,0 +1,30 @@
+package org.rackspace.gdeproxy.examples
+
+import org.junit.Test
+import org.rackspace.gdeproxy.Deproxy
+
+class DisconnectConnectorTest {
+
+    @Test
+    void testConnector() {
+
+        def deproxy = new Deproxy()
+        def endpoint = deproxy.addEndpoint(9999)
+
+//        def theProxy = new TheProxy(port: 8080,
+//                targetHostname: "localhost",
+//                targetPort: 9999)
+        def theProxy = deproxy.addEndpoint(8080, null, null, Handlers.Route("localhost", 9999, false, new DefaultClientConnector()))
+
+        def connector = new DisconnectConnector()
+
+        def mc = deproxy.makeRequest(url: "http://localhost:8080/",
+                headers: ['Host': 'localhost:8080'],
+                clientConnector: connector,
+                defaultHandler: connector.&handler)
+
+        assert mc.handlings.size() == 1
+        assert mc.handlings[0].response.code == "200"
+        assert mc.receivedResponse == null
+    }
+}

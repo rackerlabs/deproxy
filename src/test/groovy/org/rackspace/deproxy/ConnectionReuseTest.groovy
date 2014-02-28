11e670f5afa8bb2112b8b54ecddd014a66227843
Richard Sartor <richard.sartor@rackspace.com>
2014-02-28 11:40:57 -0600

    Remove apache http dependencies.

--- src/test/groovy/org/rackspace/deproxy/ConnectionReuseTest.groovy
+++ src/test/groovy/org/rackspace/deproxy/ConnectionReuseTest.groovy
@@ -12,30 +12,30 @@ class ConnectionReuseTest {
     Deproxy deproxy;
     int port;
     String url;
-    ApacheClientConnector client
+    DefaultClientConnector client;
 
     @Before
     void setUp() {
 
-        // HttpClient re-uses connection by default
-        // we don't need to roll our own code to create and re-use connections
-        client = new ApacheClientConnector()
-
-        this.deproxy = new Deproxy(null, client);
+        this.deproxy = new Deproxy();
 
         this.port = PortFinder.Singleton.getNextOpenPort();
         this.url = "http://localhost:${this.port}/";
 
-        this.deproxy.addEndpoint(this.port);
+        def endpoint = this.deproxy.addEndpoint(this.port);
+
+        def socket = (endpoint.serverConnector as SocketServerConnector).createRawConnection()
+        client = new DefaultClientConnector(socket)
+
     }
 
     @Test
     void testServerSideConnectionReuse() {
 
-        def mc1 = deproxy.makeRequest(url: url)
+        def mc1 = deproxy.makeRequest(url: url, clientConnector: client)
         assertEquals(1, mc1.handlings.size())
 
-        def mc2 = deproxy.makeRequest(url: url)
+        def mc2 = deproxy.makeRequest(url: url, clientConnector: client)
         assertEquals(1, mc2.handlings.size())
 
         assertEquals(mc1.handlings[0].connection, mc2.handlings[0].connection)

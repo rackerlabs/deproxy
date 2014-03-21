fb142aca3330fc88fb96635a0b6b700fd356f32c
David Kowis <david.kowis@rackspace.com>
2014-03-21 14:02:28 -0500

    Modified this to be "cluster safe"
    It must always use the port finder, else it will not function. The port
    could be taken already, and it could end badly.

--- src/main/groovy/org/rackspace/deproxy/LocalSocketPair.groovy
+++ src/main/groovy/org/rackspace/deproxy/LocalSocketPair.groovy
@@ -3,7 +3,20 @@ package org.rackspace.deproxy
 
 class LocalSocketPair {
 
-    public static List<Socket> createLocalSocketPair(port=null) {
+    /**
+     * I cannot find any usages of this method within any of deproxy. It's possible
+     * that it's used outside.
+     *
+     * However, it will not work with the cluster awareness, because the ports iwll conflict with ones that might be in
+     * use.
+     *
+     * This must either use the Singleton for PortFinder, or have one passed in. It will break the clustering if not
+     * @param port
+     * @return
+     */
+    public static List<Socket> createLocalSocketPair(PortFinder port=null) {
+
+        int chosenPort = -1
 
         if (port == null || port instanceof PortFinder) {
             PortFinder pf
@@ -13,11 +26,11 @@ class LocalSocketPair {
                 pf = port as PortFinder
             }
 
-            port = pf.getNextOpenPort()
+            chosenPort = pf.getNextOpenPort()
         }
 
         // create the listener socket
-        def listener = new ServerSocket(port)
+        def listener = new ServerSocket(chosenPort)
         def server
 
         // start listening on a separate thread
@@ -26,7 +39,7 @@ class LocalSocketPair {
         }
 
         // create the client socket and connect
-        def client = new Socket("localhost", port)
+        def client = new Socket("localhost", chosenPort)
 
         t.join()
 

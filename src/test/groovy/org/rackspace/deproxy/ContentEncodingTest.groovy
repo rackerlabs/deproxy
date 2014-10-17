6ae91ddfc1d4c56b0a6a1a710305249c6a34f650
Richard Sartor <richard.sartor@rackspace.com>
2014-10-17 13:48:07 -0500

    x-gzip is equivalent to gzip.

--- src/test/groovy/org/rackspace/deproxy/ContentEncodingTest.groovy
+++ src/test/groovy/org/rackspace/deproxy/ContentEncodingTest.groovy
@@ -26,4 +26,28 @@ class ContentEncodingTest {
         assertEquals(content, body)
     }
 
+    @Test
+    void testReadingXgzip() {
+
+        // given an input string, and the gzipped byte sequence for it
+        String content = "This is a string"
+        byte[] gzippedContent = content.toGzip("US-ASCII")
+        def stream = new ByteArrayInputStream(gzippedContent)
+        //  and appropriate headers describing the body and encodings
+        //  Note: according to RFC 2616 section 3.5, "x-gzip" SHOULD be
+        //  treated as equivalent to "gzip" for backwards compatibility.
+        def headers = new HeaderCollection([
+                'Content-Encoding': 'x-gzip',
+                'Content-Length': gzippedContent.length,
+                'Transfer-Encoding': 'identity'
+        ])
+
+        // when we read the gzipped data
+        def body = BodyReader.readBody(stream, headers)
+
+        // then the result should be the original string
+        assertEquals(content, body)
+
+    }
+
 }

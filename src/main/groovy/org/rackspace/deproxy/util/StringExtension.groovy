d6970d113e794937a693f126010cf949a4de19f7
Richard Sartor <richard.sartor@rackspace.com>
2014-10-17 15:26:20 -0500

    Read 'deflate' bodies.

--- src/main/groovy/org/rackspace/deproxy/util/StringExtension.groovy
+++ src/main/groovy/org/rackspace/deproxy/util/StringExtension.groovy
@@ -1,15 +1,32 @@
 package org.rackspace.deproxy.util
 
-import java.util.zip.GZIPInputStream
+import java.util.zip.DeflaterOutputStream
 import java.util.zip.GZIPOutputStream
+import java.util.zip.InflaterInputStream
 
 class StringExtension {
 
-    static byte[] toGzip(String s, String encoding="US-ASCII") {
+    static byte[] toGzip(String s, String encoding="UTF-8") {
         def byteStream = new ByteArrayOutputStream()
         def gzip = new GZIPOutputStream(byteStream)
         gzip.write(s.getBytes(encoding))
         gzip.close()
         return byteStream.toByteArray()
+    }
+
+    static byte[] toDeflate(String s, String encoding="UTF-8") {
+        def byteStream = new ByteArrayOutputStream()
+        def compress = new DeflaterOutputStream(byteStream)
+        compress.write(s.getBytes(encoding))
+        compress.close()
+        return byteStream.toByteArray()
+    }
+
+    static String inflate(byte[] bytes) {
+        def byteStream = new ByteArrayInputStream(bytes)
+        def decompress = new InflaterInputStream(byteStream)
+        def decompressedBytes = decompress.getBytes()
+        def s = new String(decompressedBytes, "UTF-8")
+        return s
     }
 }

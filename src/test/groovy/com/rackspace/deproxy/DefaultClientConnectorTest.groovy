80321fe9b33e47cf62cd795853ad7e4465c1e0b0
Richard Sartor <richard.sartor@rackspace.com>
2014-12-22 13:40:58 -0600

    Since DefaultClientConnector is no longer required to use sockets, don't use them in the tests, so as to isolate the DefaultClientConnector's behavior.

--- src/test/groovy/com/rackspace/deproxy/DefaultClientConnectorTest.groovy
+++ src/test/groovy/com/rackspace/deproxy/DefaultClientConnectorTest.groovy
@@ -6,21 +6,16 @@ import spock.lang.Unroll
 
 class DefaultClientConnectorTest extends Specification {
 
-    Socket client
-    Socket server
-
     void testConstructorWithSocketParameter() {
 
-        given: "a client socket and a server socket"
-        (client, server) = LocalSocketPair.createLocalSocketPair()
-
-        client.soTimeout = 100 // in milliseconds
-        server.soTimeout = 2000
-
-        and: "a DefaultClientConnector using the provided client socket"
-        DefaultClientConnector clientConnector =
-                new DefaultClientConnector(
-                        new BareClientConnector(client))
+        given: "a DefaultClientConnector using a dummy nextConnector"
+        Request capturedRequest = null
+        def clientConnector =
+                new DefaultClientConnector([
+                        'sendRequest': { request, https, host, port, params ->
+                            capturedRequest = request;
+                            return new Response(200)
+                        }] as ClientConnector)
 
         and: "a simple request"
         Request request = new Request("GET", "/", ['Content-Length': "0"])
@@ -30,41 +25,29 @@ class DefaultClientConnectorTest extends Specification {
 
 
         when: "we send the request through the connector"
-        try {
-
-            Response response = clientConnector.sendRequest(request, false, "localhost", server.getLocalPort(), params)
-
-        } catch (SocketTimeoutException ignored) {
-            // read times out, as expected
-        }
-
-        and: "read the request that the connector sent from the server-side socket"
-        String requestLine = LineReader.readLine(server.inputStream)
-        HeaderCollection headers = HeaderCollection.fromStream(server.inputStream)
-        String body = BodyReader.readBody(server.inputStream, headers)
-
+        Response response = clientConnector.sendRequest(request, false, "localhost", 80, params)
 
         then: "it formats the request correctly and only has the header we specified"
-        requestLine == "GET / HTTP/1.1"
-        headers.size() == 1
-        headers.contains("Content-Length")
-        headers["Content-Length"] == "0"
-        body == "" || body == null
+        capturedRequest.method == 'GET'
+        capturedRequest.path == '/'
+        capturedRequest.headers.size() == 1
+        capturedRequest.headers.contains("Content-Length")
+        capturedRequest.headers["Content-Length"] == "0"
+        capturedRequest.body == "" || capturedRequest.body == null
+        response.code == "200"
     }
 
     @Unroll("when we call sendRequest with https=#https, #host, and #port, we should get Host: #expectedValue")
     void testHostHeader() {
 
-        given: "a client socket and a server socket"
-        (client, server) = LocalSocketPair.createLocalSocketPair()
-
-        client.soTimeout = 100 // in milliseconds
-        server.soTimeout = 2000
-
-        and: "a DefaultClientConnector using the provided client socket"
-        DefaultClientConnector clientConnector =
-                new DefaultClientConnector(
-                        new BareClientConnector(client))
+        given: "a DefaultClientConnector using a dummy nextConnector"
+        Request capturedRequest = null
+        def clientConnector =
+                new DefaultClientConnector([
+                        'sendRequest': { request, https, host, port, params ->
+                            capturedRequest = request;
+                            return new Response(200)
+                        }] as ClientConnector)
 
         and: "a simple request and basic request params"
         Request request = new Request("GET", "/")
@@ -73,32 +56,16 @@ class DefaultClientConnectorTest extends Specification {
 
 
         when: "we send the request through the connector"
-        try {
-
-            // we're explicitly setting the https, host, and port parameters.
-            // the connector was created with a client socket, however. it
-            // will use the socket instead of trying to open a new connection,
-            // and just use the parameters for the Host header.
-            Response response = clientConnector.sendRequest(request, https, host, port, params)
-
-        } catch (SocketTimeoutException ignored) {
-
-            // we're expecting the connector to send the request, and then
-            // wait for a server response. since there is no server in this
-            // case, it will timeout while waiting. then we just read the
-            // request from the server side of the socket.
-        }
-
-        and: "read the request that the connector sent from the server-side socket"
-        String requestLine = LineReader.readLine(server.inputStream)
-        HeaderCollection headers = HeaderCollection.fromStream(server.inputStream)
-
-
+        Response response = clientConnector.sendRequest(request, https, host, port, params)
 
         then: "it formats the request correctly and has a Host header with the right value"
-        requestLine == "GET / HTTP/1.1"
-        headers.contains("Host")
-        headers["Host"] == expectedValue
+        capturedRequest.method == 'GET'
+        capturedRequest.path == '/'
+        capturedRequest.headers.contains("Host")
+        capturedRequest.headers["Host"] == expectedValue
+        response.code == "200"
+
+
 
         where:
         host          | port  | https | expectedValue
@@ -120,14 +87,5 @@ class DefaultClientConnectorTest extends Specification {
         "12.34.56.78" | 443   | false | "12.34.56.78:443"
         "12.34.56.78" | 12345 | true  | "12.34.56.78:12345"
         "12.34.56.78" | 12345 | false | "12.34.56.78:12345"
-    }
-
-    def cleanup() {
-        if (client) {
-            client.close()
-        }
-        if (server) {
-            server.close()
-        }
     }
 }
